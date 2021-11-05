3ca04ad6df0d13bb21715cba613189e93c37c8bb
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-11-05 23:28:06 +0300

    Add documentation comments to ImageUploader protocol

--- ImgurCore/ImageUploader.swift
+++ ImgurCore/ImageUploader.swift
@@ -6,8 +6,14 @@
 //  Copyright © 2021 Anton Ilinykh. All rights reserved.
 //
 
-import Foundation
-
+/// A type that performs image file uploading to the remote server
 public protocol ImageUploader {
+    /// Uploads file to remote location
+    ///
+    /// some discussion info
+    ///
+    /// - Parameters:
+    ///     - image: The local image
+    ///     - completion: The completion handler that provides the ``RemoteImage`` in case of succes and an Error in case of failure
     func upload(image: LocalImage, completion: @escaping (RemoteImage?, Error?) -> Void)
 }
