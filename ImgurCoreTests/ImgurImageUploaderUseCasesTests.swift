9c529efba1556d7ebb184e5375306540e061fe47
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-11-06 22:48:29 +0300

    Test upload method sets Authorization header for HTTP request

--- ImgurCoreTests/ImgurImageUploaderUseCasesTests.swift
+++ ImgurCoreTests/ImgurImageUploaderUseCasesTests.swift
@@ -11,21 +11,48 @@ import ImgurCore
 
 final class ImgurImageUploader: ImageUploader {
     
-    func upload(from: URL, completion: @escaping (ImageUploader.Result) -> Void) {
-        let url = URL(string: "a-remote-image-url")!
-        completion(.success(url))
+    let client: HTTPClient
+    let clientId: String
+    
+    init(client: HTTPClient, clientId: String) {
+        self.client = client
+        self.clientId = clientId
+    }
+    
+    func upload(url: URL, completion: @escaping (ImageUploader.Result) -> Void) {
+        var request = URLRequest(url: url)
+        request.setValue("Client-ID \(clientId)", forHTTPHeaderField: "Authorization")
+        client.perform(request: request) { result in
+            switch result {
+            case .success(let (data, response)):
+                completion(.success(URL(string: "a-remote-image-url")!))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
 
 class ImgurImageUploaderUseCasesTests: XCTestCase {
+    
+    func test_upload_setAuthorizationHeader() {
+        let (sut, client) = makeSUT()
+        let fileUrl = URL(fileURLWithPath: "a-path")
+        
+        sut.upload(url: fileUrl) { _ in }
+        client.complete(with: Data(), response: HTTPURLResponse())
+        
+        let request = client.getRequest()
+        XCTAssertNotNil(request.value(forHTTPHeaderField: "Authorization"))
+    }
 
     func test_upload_deliversRemoteImageURL() {
-        let sut = ImgurImageUploader()
+        let (sut, client) = makeSUT()
         let fileUrl = URL(fileURLWithPath: "a-path")
         let remoteImageUrl = URL(string: "a-remote-image-url")!
         let exp = XCTestExpectation(description: "image upload expectation")
         
-        sut.upload(from: fileUrl) { result in
+        sut.upload(url: fileUrl) { result in
             switch result {
             case .failure:
                 XCTFail("Expected succes but gor error")
@@ -34,8 +61,31 @@ class ImgurImageUploaderUseCasesTests: XCTestCase {
             }
             exp.fulfill()
         }
+        client.complete(with: Data(), response: HTTPURLResponse())
         
         wait(for: [exp], timeout: 0.1)
     }
 
+    private func makeSUT() -> (sut: ImgurImageUploader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = ImgurImageUploader(client: client, clientId: "CLIENT_ID")
+        return (sut, client)
+    }
+}
+
+
+private final class HTTPClientSpy: HTTPClient {
+    private var messages = [(request: URLRequest, completion: (HTTPClient.Result) -> Void)]()
+    
+    func perform(request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) {
+        messages.append((request, completion))
+    }
+    
+    func getRequest(at index: Int = 0) -> URLRequest {
+        return messages[index].request
+    }
+    
+    func complete(with data: Data, response: HTTPURLResponse, at index: Int = 0) {
+        messages[index].completion(.success((data, response)))
+    }
 }
