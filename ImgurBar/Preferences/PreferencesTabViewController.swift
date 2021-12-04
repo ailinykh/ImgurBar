30b561c0bb354658c53c32a019c0f799826d8ec4
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-12-04 22:02:20 +0300

    Move PreferencesTabViewController into separate file

---
+++ ImgurBar/Preferences/PreferencesTabViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2021 Anton Ilinykh. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesTabViewController: NSTabViewController {
+    override func tabView(_ tabView: NSTabView, didSelect tabViewItem: NSTabViewItem?) {
+        // FIXME: By default tabViewItem's size is 500x500
+        // https://stackoverflow.com/a/54379220
+        if let tabViewItem = tabViewItem, let viewController = tabViewItem.viewController {
+            viewController.preferredContentSize = viewController.view.frame.size
+        }
+        super.tabView(tabView, didSelect: tabViewItem)
+    }
+}
--- ImgurBar/Preferences/PreferencesWindowController.swift
+++ ImgurBar/Preferences/PreferencesWindowController.swift
@@ -14,14 +14,3 @@ class PreferencesWindowController: NSWindowController {
         super.windowDidLoad()
     }
 }
-
-class PreferencesTabController: NSTabViewController {
-    override func tabView(_ tabView: NSTabView, didSelect tabViewItem: NSTabViewItem?) {
-        // FIXME: By default tabViewItem's size is 500x500
-        // https://stackoverflow.com/a/54379220
-        if let tabViewItem = tabViewItem, let viewController = tabViewItem.viewController {
-            viewController.preferredContentSize = viewController.view.frame.size
-        }
-        super.tabView(tabView, didSelect: tabViewItem)
-    }
-}
