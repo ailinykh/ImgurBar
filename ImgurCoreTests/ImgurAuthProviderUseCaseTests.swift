b7d23b0a8d6b08deeda44cb09f639088cc5713a8
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-12-27 00:22:53 +0300

    Add client_id and auth client as a dependencies to imgur auth provider

--- ImgurCoreTests/ImgurAuthProviderUseCaseTests.swift
+++ ImgurCoreTests/ImgurAuthProviderUseCaseTests.swift
@@ -6,16 +6,41 @@ import XCTest
 import ImgurCore
 
 class ImgurAuthProvider: AuthProvider {
+    
+    let clientId: String
+    let client: AuthClient
+    
+    enum ImgurAuthError: Error {
+        case invalidUrlRedirect
+        case insufficientParams
+    }
+    
+    init(clientId: String, client: AuthClient) {
+        self.clientId = clientId
+        self.client = client
+    }
+    
     func authorize(completion: @escaping (Result<AuthData, Error>) -> Void) {
-        let error = NSError(domain: "unknown", code: 0)
-        completion(.failure(error))
+        let url = URL(string:"https://api.imgur.com/oauth2/authorize?client_id=\(clientId)&response_type=token")!
+        client.open(url: url) { _ in
+            let error = NSError(domain: "unknown", code: 0)
+            completion(.failure(error))
+        }
+    }
+}
+
+private final class AuthClientStub: AuthClient {
+    func open(url: URL, completion: @escaping (Result<URL, Error>) -> Void) {
+        completion(.success(url))
     }
 }
 
 class ImgurAuthProviderUseCaseTests: XCTestCase {
 
     func test_authorize_deliversAuthResult() {
-        let sut = ImgurAuthProvider()
+        let clientId = "some_client_id"
+        let client = AuthClientStub()
+        let sut = ImgurAuthProvider(clientId: clientId, client: client)
         let exp = expectation(description: "Authorization expectation")
         
         sut.authorize { result in
