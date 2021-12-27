a9971d0b0735c97145d7e7304f4efc251fb2eaf2
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-12-27 11:58:13 +0300

    Test imgur auth provider with auth client

--- ImgurCoreTests/ImgurAuthProviderUseCaseTests.swift
+++ ImgurCoreTests/ImgurAuthProviderUseCaseTests.swift
@@ -30,20 +30,29 @@ class ImgurAuthProvider: AuthProvider {
 }
 
 private final class AuthClientStub: AuthClient {
+    private var completions = [(Result<URL, Error>) -> Void]()
+    
     func open(url: URL, completion: @escaping (Result<URL, Error>) -> Void) {
-        completion(.success(url))
+        completions.append(completion)
+    }
+    
+    func complete(with result: Result<URL, Error>, at: Int = 0) {
+        completions[at](result)
     }
 }
 
 class ImgurAuthProviderUseCaseTests: XCTestCase {
 
-    func test_authorize_deliversAuthResult() {
-        let (sut, _) = makeSUT()
+    func test_authorize_deliversAnyAuthResult() {
+        let (sut, client) = makeSUT()
         let exp = expectation(description: "Authorization expectation")
         
         sut.authorize { result in
             exp.fulfill()
         }
+        
+        let url = URL(string: "https://an-url.com")!
+        client.complete(with: .success(url))
         
         wait(for: [exp], timeout: 0.1)
     }
@@ -51,9 +60,8 @@ class ImgurAuthProviderUseCaseTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT() -> (ImgurAuthProvider, AuthClientStub) {
-        let clientId = "some_client_id"
         let client = AuthClientStub()
-        let sut = ImgurAuthProvider(clientId: clientId, client: client)
+        let sut = ImgurAuthProvider(clientId: "some_client_id", client: client)
         return (sut, client)
     }
 }
