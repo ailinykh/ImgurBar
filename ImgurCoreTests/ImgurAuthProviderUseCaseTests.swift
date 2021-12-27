59c6c0b3e23880a3e53ad64879cc11b0a44c97d1
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-12-27 13:07:01 +0300

    Add more test cases for imgur auth provider

--- ImgurCoreTests/ImgurAuthProviderUseCaseTests.swift
+++ ImgurCoreTests/ImgurAuthProviderUseCaseTests.swift
@@ -10,7 +10,7 @@ class ImgurAuthProvider: AuthProvider {
     let clientId: String
     let client: AuthClient
     
-    enum ImgurAuthError: Error {
+    enum Error: Swift.Error {
         case invalidUrlRedirect
         case insufficientParams
     }
@@ -20,13 +20,49 @@ class ImgurAuthProvider: AuthProvider {
         self.client = client
     }
     
-    func authorize(completion: @escaping (Result<AuthData, Error>) -> Void) {
-        let url = URL(string:"https://api.imgur.com/oauth2/authorize?client_id=\(clientId)&response_type=token")!
-        client.open(url: url) { _ in
-            let error = NSError(domain: "unknown", code: 0)
-            completion(.failure(error))
+    func authorize(completion: @escaping (Result<AuthData, Swift.Error>) -> Void) {
+            let url = URL(string:"https://api.imgur.com/oauth2/authorize?client_id=\(clientId)&response_type=token")!
+            client.open(url: url) { [weak self] result in
+                switch result {
+                case .success(let url):
+                    do {
+                        let authData = try self!.handle(url: url)
+                        completion(.success(authData))
+                    } catch {
+                        completion(.failure(error))
+                    }
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
         }
-    }
+        
+        private func handle(url: URL) throws -> AuthData {
+            let parts = url.absoluteString.split(separator: "#")
+            guard parts.count > 1, let path = parts.last
+            else {
+                throw Error.invalidUrlRedirect
+            }
+            
+            let dict = parse(path: String(path))
+            guard let token = dict["access_token"], let account = dict["account_username"] else {
+                throw Error.insufficientParams
+            }
+            
+            return AuthData(accessToken: token, accountName: account)
+        }
+        
+        private func parse(path: String) -> [String: String] {
+            return path.split(separator: "&")
+                .reduce([String:String](), { partialResult, part in
+                    var partialResult = partialResult
+                    let parts = part.split(separator: "=")
+                    if let key = parts.first, let value = parts.last {
+                        partialResult[String(key)] = String(value)
+                    }
+                    return partialResult
+                })
+        }
 }
 
 private final class AuthClientStub: AuthClient {
@@ -43,7 +79,7 @@ private final class AuthClientStub: AuthClient {
 
 class ImgurAuthProviderUseCaseTests: XCTestCase {
 
-    func test_authorize_deliversAnyAuthResult() {
+    func test_authorize_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
         let error = NSError(domain: "unknown", code: 0)
@@ -52,7 +88,25 @@ class ImgurAuthProviderUseCaseTests: XCTestCase {
         }
     }
     
-    private func expect(_ sut: ImgurAuthProvider, toCompleteWith expectedResult: AuthProvider.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+    func test_authorize_deliversErrorOnInsufficientParams() {
+        let (sut, client) = makeSUT()
+        
+        expect(sut, toCompleteWith: .failure(ImgurAuthProvider.Error.insufficientParams)) {
+            let url = URL(string: "https://an-url.com/#access_token=")!
+            client.complete(with: .success(url))
+        }
+    }
+    
+    func test_authorize_deliversAuthData() {
+        let (sut, client) = makeSUT()
+        let url = URL(string: "https://an-url.com/#access_token=some-token&account_username=some-name")!
+        let authData = AuthData(accessToken: "some-token", accountName: "some-name")
+        expect(sut, toCompleteWith: .success(authData)) {
+            client.complete(with: .success(url))
+        }
+    }
+    
+    private func expect(_ sut: ImgurAuthProvider, toCompleteWith expectedResult: Result<AuthData, Error>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "Authorization expectation")
         sut.authorize { receivedResult in
             switch (expectedResult, receivedResult) {
