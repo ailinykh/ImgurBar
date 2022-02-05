26c72f6878e63691721ea36383cb8c88648b5689
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-02-05 22:16:54 +0300

    Test `load` delivers albums on client success

--- ImgurCore/Album.swift
+++ ImgurCore/Album.swift
@@ -7,4 +7,9 @@ import Foundation
 public struct Album: Equatable {
     public let id: String
     public let title: String
+    
+    public init(id: String, title: String) {
+        self.id = id
+        self.title = title
+    }
 }
--- ImgurCoreTests/ImgurAlbumLoaderUsecaseTests.swift
+++ ImgurCoreTests/ImgurAlbumLoaderUsecaseTests.swift
@@ -10,6 +10,17 @@ class ImgurAlbumLoader: AlbumLoader {
         case invalidData
     }
     
+    struct Response: Decodable {
+        struct RemoteAlbum: Decodable {
+            let id: String
+            let title: String
+        }
+        
+        var data: [RemoteAlbum]
+        var success: Bool
+        var status: Int
+    }
+    
     let client: HTTPClient
     
     init(client: HTTPClient) {
@@ -20,13 +31,16 @@ class ImgurAlbumLoader: AlbumLoader {
         var request = URLRequest(url: URL(string: "https://an-url")!)
         request.setValue("Bearer: \(account.token)", forHTTPHeaderField: "Authorization")
         client.perform(request: request) { result in
-            completion(.success([]))
+            if let (data, _) = try? result.get() {
+                let response = try! JSONDecoder().decode(Response.self, from: data)
+                completion(.success(response.data.map({Album(id: $0.id, title: $0.title)})))
+            }
         }
     }
 }
 
 class ImgurAlbumLoaderUsecaseTests: XCTestCase {
-    func test_albumLoaderSetsAuthorizationTokenForRequest() {
+    func test_loadSetsAuthorizationTokenForRequest() {
         let (sut, client) = makeSUT()
         let account = Account(token: "some-token", username: "some-username")
         
@@ -37,7 +51,31 @@ class ImgurAlbumLoaderUsecaseTests: XCTestCase {
         XCTAssertEqual(authorization, "Bearer: some-token")
     }
     
+    func test_loadDeliversAlbumsOnSuccess() {
+        let (sut, client) = makeSUT()
+        let albums = [
+            Album(id: "album1Id", title: "album1Title"),
+            Album(id: "album2Id", title: "album2Title"),
+        ]
+        
+        expect(sut, toCompleteWith: .success(albums)) {
+            client.complete(with: makeResponseData(for: albums), response: .any)
+        }
+    }
+    
     //MARK: - Helpers
+    
+    private func makeResponseData(for albums: [Album], status: Int = 0) -> Data {
+        let json = [
+            "data": albums.map {
+                ["id": $0.id, "title": $0.title]
+            },
+            "success": status == 0,
+            "status": status
+        ].compactMapValues { $0 }
+        
+        return try! JSONSerialization.data(withJSONObject: json)
+    }
     
     private func makeSUT() -> (sut: ImgurAlbumLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
