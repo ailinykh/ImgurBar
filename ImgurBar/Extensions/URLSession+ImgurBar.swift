bf93420d2e49d21777847b3ed1b58bec430961d3
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-02-06 19:30:28 +0300

    Extend `URLSession` with `performDataTask` method to make non-upload requests

--- ImgurBar/Extensions/URLSession+ImgurBar.swift
+++ ImgurBar/Extensions/URLSession+ImgurBar.swift
@@ -7,9 +7,29 @@ import ImgurCore
 
 extension URLSession: HTTPClient {
     public func perform(request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) {
+        if let httpMethod = request.httpMethod, httpMethod == "POST" {
+            performUploadTask(for: request, completion: completion)
+        } else {
+            performDataTask(for: request, completion: completion)
+        }
+    }
+    
+    private func performUploadTask(for request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) {
         uploadTask(with: request, from: request.httpBody) { data, response, error in
             guard let data = data, let response = response else {
-                completion(.failure(error ?? NSError(domain: "unknown error", code: 0)))
+                let unknown = NSError(domain: "unknown error", code: -1)
+                completion(.failure(error ?? unknown))
+                return
+            }
+            completion(.success((data, response)))
+        }.resume()
+    }
+    
+    private func performDataTask(for request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) {
+        dataTask(with: request) { data, response, error in
+            guard let data = data, let response = response else {
+                let unknown = NSError(domain: "unknown error", code: -1)
+                completion(.failure(error ?? unknown))
                 return
             }
             completion(.success((data, response)))
